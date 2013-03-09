ca9df50a36170ef1bebddea85211c5a52ce429d6
Maciej Pasternacki <maciej@pasternacki.net>
2013-03-09 21:31:10 +0100

    Java options and current directory for the daemons.

--- attributes/default.rb
+++ attributes/default.rb
@@ -4,6 +4,10 @@ default['logstash_stack']['elasticsearch']['config']['node.data'] = true
 
 default['logstash_stack']['redis']['use_distribution_packages'] = false
 
-default['logstash_stack']['indexer'] = {}
-default['logstash_stack']['shipper'] = {}
+default['logstash_stack']['indexer']['java_opts'] = '-server -Djava.awt.headless=true -Xmx512M -Xms128M'
+
+default['logstash_stack']['web']['java_opts'] = '-server -Djava.awt.headless=true -Xmx256M -Xms64M'
 default['logstash_stack']['web']['domain'] = (fqdn rescue nil)
+
+
+default['logstash_stack']['shipper']['java_opts'] = '-server -Djava.awt.headless=true -Xmx32M -Xms4M'
