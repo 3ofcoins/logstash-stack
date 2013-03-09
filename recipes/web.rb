951e731fd39504a7b89eb45b868d6721a27a7aea
Maciej Pasternacki <maciej@pasternacki.net>
2013-03-09 21:30:32 +0100

    Provide Elasticsearch IP to logstash-web.

--- recipes/web.rb
+++ recipes/web.rb
@@ -4,8 +4,14 @@ include_recipe 'apache2::mod_proxy_http'
 
 include_recipe 'logstash-stack::_common'
 
+elasticsearch_ip = tagged?('logstash-elasticsearch') ? '127.0.0.1' :
+  search(:node,
+    "tags:logstash-elasticsearch AND chef_environment:#{node.chef_environment}"
+  ).first['ipaddress']
+
 runit_service 'logstash-web' do
   default_logger true
+  options :elasticsearch => elasticsearch_ip
 end
 
 template "#{node['apache']['dir']}/sites-available/logstash.conf" do
