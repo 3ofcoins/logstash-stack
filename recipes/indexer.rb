787b7551bafb41074d0f0f59aaeda2d9de7bdaf6
Maciej Pasternacki <maciej@3ofcoins.net>
2013-12-07 22:38:43 +0100

    Correct condition

--- recipes/indexer.rb
+++ recipes/indexer.rb
@@ -17,7 +17,7 @@ end
 execute 'generate lumberjack key' do
   command "openssl req -x509 -newkey rsa:2048 -keyout /srv/logstash/indexer-lumberjack.key -out /srv/logstash/indexer-lumberjack.pub -nodes -days 3650 -subj /CN=#{node['fqdn']}"
   creates '/srv/logstash/indexer-lumberjack.key'
-  only_if { node['logstash']['indexer']['input']['lumberjack'] }
+  only_if { node['logstash']['indexer']['input']['50-lumberjack'] }
   user 'root'
   group 'logstash'
   umask '0027'
@@ -27,7 +27,7 @@ ruby_block 'save lumberjack key' do
   block do
     node.set['logstash_stack']['lumberjack']['public_key'] = File.read('/srv/logstash/indexer-lumberjack.pub')
   end
-  only_if { node['logstash']['indexer']['input']['lumberjack'] }
+  only_if { node['logstash']['indexer']['input']['50-lumberjack'] }
 end
 
 logstash_agent 'indexer'
