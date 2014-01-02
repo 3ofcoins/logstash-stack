f226d2df5b83d39e426defda4bb5fa7f17aab64a
Maciej Pasternacki <maciej@3ofcoins.net>
2014-01-02 13:01:59 +0100

    Don't manage template, Logstash 1.3.2+ takes care of that.

--- recipes/elasticsearch.rb
+++ recipes/elasticsearch.rb
@@ -19,25 +19,4 @@ file '/etc/elasticsearch/elasticsearch.yml' do
   notifies :restart, 'service[elasticsearch]'
 end
 
-# https://gist.github.com/deverton/2970285
-http_request 'elasticsearch::template_logstash' do
-  action :put
-  url 'http://localhost:9200/_template/template_logstash/'
-  message(
-    template: "logstash*",
-    settings: {
-      "index.query.default_field" => "message",
-      "index.cache.field.type" => "soft",
-      "index.store.compress.stored" => true },
-    mappings: {
-      _default_: {
-        _all: { enabled: false },
-        properties: {
-          "message" =>    { type: "string", index: "analyzed" },
-          "@version" =>   { type: "string", index: "not_analyzed" },
-          "@timestamp" => { type: "date", index: "not_analyzed" },
-          "type" =>       { type: "string", index: "not_analyzed" }
-        }}})
-end
-
 tag 'logstash-elasticsearch'
